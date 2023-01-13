f2a9a4d9ea134fbf67265db55a75e921bc737d7e
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-01-13 20:41:26 +0100

    feat: fix dependencies path update

--- projects/ngx-lib/src/public-api.ts
+++ projects/ngx-lib/src/public-api.ts
@@ -5,7 +5,3 @@
 export * from './lib/ngx-lib.service';
 export * from './lib/ngx-lib.component';
 export * from './lib/ngx-lib.module';
-export * from '../../design-system/src/app/app.module';
-export * from '../../design-system/src/app/app.component';
-export * from '../../design-system/src/app/app-routing.module';
-export * from '../../design-system/src/app/app-routing.module';
